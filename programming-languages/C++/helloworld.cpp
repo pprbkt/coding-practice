2b2a010252617386e2253751f604abcc5d4077e9
pprbkt <dhanushhs1@outlook.com>
2026-02-10 12:12:34 +0530

    learnt about const

--- programming-languages/C++/helloworld.cpp
+++ programming-languages/C++/helloworld.cpp
@@ -15,7 +15,7 @@ int main(){
     int year = 2024; 
     int days = 365; 
     
-    int days2 = 7.5; // This line will cause a compilation error because 7.5 is a floating-point number and cannot be assigned to an integer variable
+    /*int days2 = 7.5; // This line will cause a compilation error because 7.5 is a floating-point number and cannot be assigned to an integer variable*/
 
     double price = 19.99; // Declaring and initializing a double variable named price with the value 19.99
     double temperature = 36.5;
@@ -25,7 +25,7 @@ int main(){
     char initial = 'J'; 
     char symbol = '@';
 
-    char letters = 'AB'; // This line will cause a compilation error because 'AB' is a string literal and cannot be assigned to a char variable
+    /*char letters = 'AB'; // This line will cause a compilation error because 'AB' is a string literal and cannot be assigned to a char variable*/
 
     bool isRaining = true; // Declaring and initializing a bool variable named isRaining with the value true
     bool isSunny = false; 
@@ -36,6 +36,13 @@ int main(){
     std::string country = "USA"; 
 
     std::cout << "Name: " << name << std::endl; // Output the value of the name variable to the console
-    
+
+    /* const keyword: The const keyword is used to declare a variable as constant, meaning its value cannot be changed after it has been initialized*/
+    const double PI = 3.14159; // Declaring a constant variable named PI with the value 3.14159
+    double radius = 5.0; 
+    double circumference = 2 * PI * radius; 
+    std::cout << "The circumference of the circle is: " << circumference << std::endl;
+
+
     return 0; // Return 0 to indicate that the program ended successfully
 }
